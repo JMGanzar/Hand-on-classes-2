a51b91d89b3f414f6807111c8284fb29ffdb9032
Jose Manuel Gandia Zaragoza <jose.gandia02@goumh.umh.es>
2025-04-01 18:48:45 +0200

    Add existsByAdmin method in UsuarioRepository

--- src/main/java/todolist/repository/UsuarioRepository.java
+++ src/main/java/todolist/repository/UsuarioRepository.java
@@ -2,9 +2,11 @@ package todolist.repository;
 
 import todolist.model.Usuario;
 import org.springframework.data.repository.CrudRepository;
-
 import java.util.Optional;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
-    Optional<Usuario> findByEmail(String s);
+    Optional<Usuario> findByEmail(String email);
+
+    // Método para verificar si existe un usuario con el campo admin = true/false
+    boolean existsByAdmin(boolean admin);
 }
